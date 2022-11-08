d8a942ffa093a89a97452c47637c2178f8dc3df7
10Sadique <jafarsjahan@gmail.com>
2022-11-08 13:24:43 +0600

    mongodb conntected

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // port
 const port = process.env.PORT;
@@ -14,6 +15,34 @@ app.use(cors());
 app.use(express.json());
 
 // mongoDB
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.onfc57d.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    const database = client.db('soloCarpentry');
+    const serviceCollection = database.collection('services');
+
+    // services
+    app.get('/services', async (req, res) => {
+        let query = {};
+        let size = Infinity;
+
+        if (req.query.limit) {
+            size = +req.query.limit;
+        }
+        const cursor = serviceCollection.find(query);
+        const services = await cursor.limit(size).toArray();
+
+        res.send(services);
+    });
+}
+
+run().catch((err) => console.log(err));
 
 // init server
 app.get('/', (req, res) => {
